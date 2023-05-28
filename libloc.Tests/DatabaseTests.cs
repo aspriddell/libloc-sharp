3c41449d658036120d45ff76d57b801114a1c6c0
Albie <albion@dragonfruit.network>
2023-05-28 11:56:54 +0100

    update remaining tests

--- libloc.Tests/DatabaseTests.cs
+++ libloc.Tests/DatabaseTests.cs
@@ -29,20 +29,45 @@ namespace libloc.Tests
             await locationDbService.StopAsync(timeout.Token);
         }
 
+        [TestCase("140.238.99.135", "140.238.96.0", 31898)]
+        [TestCase("2001:67c:2e8:22::c100:691", "2001:67c:2e8::", 3333)]
         [TestCase("2a00:23c7:acb:ef01::", "2a00:2380::", 2856)]
         public async Task TestAddressResolution(string address, string expectedStartBlock, int expectedAs)
         {
-            if (!IPAddress.TryParse(address, out var targetAddress))
-            {
-                Assert.Inconclusive($"{nameof(address)} was not a valid address");
-            }
-
             var database = TestServices.Services.GetRequiredService<ILocationDbAccessor>();
-            var addressInfo = await database.PerformAsync(db => db.ResolveAddress(targetAddress)).ConfigureAwait(false);
+            var addressInfo = await database.PerformAsync(db => db.ResolveAddress(IPAddress.Parse(address))).ConfigureAwait(false);
 
             Assert.IsNotNull(addressInfo);
             Assert.That(addressInfo.ASN, Is.EqualTo(expectedAs));
-            Assert.That(addressInfo.FirstAddress, Is.EqualTo(IPAddress.Parse(expectedStartBlock)));
+            Assert.That(addressInfo.FirstAddress, Is.EqualTo(IPAddress.Parse(expectedStartBlock).MapToIPv6()));
+        }
+
+        [TestCase("10.11.12.13")]
+        [TestCase("127.0.20.100")]
+        [TestCase("fc00::2001:abcd:0010")]
+        public async Task TestUnassignedAddressResolution(string address)
+        {
+            if (!IPAddress.TryParse(address, out var ipAddress))
+            {
+                Assert.Inconclusive();
+                return;
+            }
+
+            var database = TestServices.Services.GetRequiredService<ILocationDbAccessor>();
+            var addressInfo = await database.PerformAsync(db => db.ResolveAddress(ipAddress));
+
+            Assert.IsNull(addressInfo);
+        }
+
+        [TestCase(786U)]
+        [TestCase(3333U)]
+        public async Task TestASLookup(uint asn)
+        {
+            var database = TestServices.Services.GetRequiredService<ILocationDbAccessor>();
+            var asInfo = await database.PerformAsync(db => db.AS[asn]);
+
+            Assert.IsNotNull(asInfo);
+            Assert.IsNotEmpty(asInfo.Name);
         }
     }
 }
